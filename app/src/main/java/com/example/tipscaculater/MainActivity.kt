779b83cc5732f28a83bfdff4def924ba78ad4da4
BrunoTavaresQA <115037702+BrunoTavaresQA@users.noreply.github.com>
2024-05-13 16:36:04 -0300

    Usando FindViewById

--- app/src/main/java/com/example/tipscaculater/MainActivity.kt
+++ app/src/main/java/com/example/tipscaculater/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.tipscaculater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLimpar: Button =  findViewById(R.id.btn_limpar)
+        val btnCalcular: Button =  findViewById(R.id.btn_calcular)
+        val edtTotal: TextInputEditText = findViewById(R.id.tie_total)
+        val edtPessoas: TextInputEditText = findViewById(R.id.tie_pessoas)
+
+        btnLimpar.setOnClickListener{
+            println("Bruno1 " + edtTotal.text)
+            println("Bruno1 " + edtPessoas.text)
+        }
     }
 }
